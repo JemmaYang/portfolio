ebf5b377b9861a01b2f704e7842ebb95083e47a0
jemma_yang <jessicyoungvive@163.com>
2017-01-25 22:28:19 -0500

    update

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+// float back-top buttom
 var amountScrolled = 250;
 
 $(window).scroll(function() {
@@ -15,25 +17,3 @@ $('a.back-top').click(function() {
 	return false;
 });
 
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-
-var myIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 2000); // Change image every 2 seconds
-}
